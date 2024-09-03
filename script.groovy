24792956c77ad2f4e35407d8c763a420e1be39ae
loai-96 <loai.e.zayed@gmail.com>
2024-09-03 21:09:20 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,11 +5,11 @@ def builJar() {
 
 def buildImage() {
      echo 'Building the docker image...'
-                    /* groovylint-disable-next-line LineLength */
-                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        sh 'docker build -t loaiezayed925/my-repo:jma-2.0 .'
-                        sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh 'docker push loaiezayed925/my-repo:jma-2.0 '
-                    }
+    /* groovylint-disable-next-line LineLength */
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t loaiezayed925/my-repo:jma-2.0 .'
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'docker push loaiezayed925/my-repo:jma-2.0 '
+    }
 }
 return this
