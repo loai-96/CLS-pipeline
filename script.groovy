906fe3b759405275a802eff74408e2a41d7f0a2f
loai-96 <loai.e.zayed@gmail.com>
2024-09-03 20:55:06 +0300

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def builJar() {
+     echo "Building the application..."
+     sh 'mvn package'
+}
+
+def buildImage() {
+     echo 'Building the docker image...'
+                    /* groovylint-disable-next-line LineLength */
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh 'docker build -t loaiezayed925/my-repo:jma-2.0 .'
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh 'docker push loaiezayed925/my-repo:jma-2.0 '
+                    }
+}
+return this
